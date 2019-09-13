ffbef12f9c96d6ac047131feb26167b0d2fa222b
Anton <anton@binary.com>
2019-09-13 13:24:00 +0800

    Updates

---
+++ br_helpcontent/js/bot/11-6c55f0.bot.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpderiv_bot=window.webpackJsonpderiv_bot||[]).push([[11],{550:function(o){o.exports=JSON.parse('{"text":["You can use this block to purchase the contract of a specific type you define in the Trade Parameters block. You can add multiple Purchase blocks in conjunction with conditional blocks to define purchase conditions. You can use this block only within the Purchase conditions block."]}')}}]);
+//# sourceMappingURL=11-6c55f0.bot.js.map
