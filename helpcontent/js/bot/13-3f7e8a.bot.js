3e80f0ad85bc6b0e999048bd8b41fe8f6ab40667
Anton <anton@binary.com>
2019-09-13 12:04:45 +0800

    Updates

---
+++ helpcontent/js/bot/13-3f7e8a.bot.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpderiv_bot=window.webpackJsonpderiv_bot||[]).push([[13],{552:function(t){t.exports=JSON.parse('{"text":["This block sells your active contract at market price. You don\'t necessarily need to sell your contract, however you might want to do it if the market trend is against you.","Example:"]}')}}]);
+//# sourceMappingURL=13-3f7e8a.bot.js.map
---
+++ helpcontent/js/bot/14-ca4055.bot.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpderiv_bot=window.webpackJsonpderiv_bot||[]).push([[14],{553:function(e){e.exports=JSON.parse('{"text":["This blocks should be placed outside of any root block. Instructions placed inside this block will be executed whenever new tick comes. E.g. in case if default time interval is set to 1 minute in the root block “Define your trade contract” then instructions will be executed every 1 minute.","Example:"]}')}}]);
+//# sourceMappingURL=14-ca4055.bot.js.map
