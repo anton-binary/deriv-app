b5c4751f179141f722d1bb3eac4c2b65600a5640
Anton <anton@binary.com>
2019-11-13 10:40:51 +0800

    Updates

---
+++ bot_block-shapes/service-worker.js
@@ -0,0 +1,33 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("/bot_block-shapes/workbox-v4.3.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "/bot_block-shapes/workbox-v4.3.1"});
+
+importScripts(
+  "/bot_block-shapes/precache-manifest.501cf3c2fa783802027e54c76794e90a.js"
+);
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.precaching.cleanupOutdatedCaches();
