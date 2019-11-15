3805916f8fc7a12500172927d8c3740636c1f891
Anton <anton@binary.com>
2019-11-15 10:18:00 +0800

    Updates

--- bot_block-shapes/service-worker.js
+++ bot_block-shapes/service-worker.js
@@ -15,7 +15,7 @@ importScripts("/bot_block-shapes/workbox-v4.3.1/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/bot_block-shapes/workbox-v4.3.1"});
 
 importScripts(
-  "/bot_block-shapes/precache-manifest.8c4e502fb9d5d5a246c27ce49254f12a.js"
+  "/bot_block-shapes/precache-manifest.a88909cc37e657a45431586fdd5edd18.js"
 );
 
 workbox.core.skipWaiting();
